192cd918ff585e9e524a6589ad7788afd94554ed
Egggggy <louisl0517l@gmail.com>
2026-04-21 17:53:50 +0800

    feat:調整姿態

--- triceratops_quadruped_robot/triceratops_base/Controller.py
+++ triceratops_quadruped_robot/triceratops_base/Controller.py
@@ -295,8 +295,8 @@ class ControlCmd:
     def motor_position_control(self, position=None, waist=None):
         if position is None:
             position = [[2048, 2048, 2048, 2048],
-                        [1992, 2047, 2092, 2099],
-                        [2600, 2600, 2600, 2600]]
+                        [2048, 2047, 2048, 2048],
+                        [2048, 2048, 2048, 2048]]
 
         for i, motor_list in enumerate(self.leg_motor_list):
             for j, motor in enumerate(motor_list):
